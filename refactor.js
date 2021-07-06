d35ca577d8f7d3913fd0ba0890033d8bbbfc6814
esitamon <sitamoe_2010@yahoo.fr>
2021-07-06 17:39:42 -0500

    second commit

---
+++ refactor.js
@@ -0,0 +1,45 @@
+class game {
+
+    constructor(userInput){
+
+        this.userInput = userInput;
+    }
+
+    set userinput(x){
+        if(x > 13 &&  x < 1){
+
+            this.userInput = x ;
+        }
+    }
+
+    
+    
+   aiNumber(){
+    let gameArray = [1,4,5,6,7,8,9,10,11,12,13];
+    let min , max ; [min, max] = [1,10];
+
+    let randNum = Math.random(max);
+    let randValue = Math.floor((randNum*max)+1);
+    
+    return gameArray[randValue];
+
+   }
+
+    playAgame(){
+        let gameArray = [1,4,5,6,7,8,9,10,11,12,13];
+        let aiVal = this.aiNumber(gameArray);
+        console.log(aiVal);
+        
+        if (this.userInput == aiVal){
+            alert("Congrat you beat the computer");
+        }else{
+            alert("You missed , the computer pick "+ aiVal);
+            console.log(aiVal);
+        }
+
+    }
+}
+
+let userinput = prompt("Please pick a number 1-13: ");
+let newGame = new game(userinput);
+newGame.playAgame();
